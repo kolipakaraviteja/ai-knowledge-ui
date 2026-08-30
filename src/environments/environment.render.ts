5c162ab49e621eaa37aa64f466ed0028035fd15d
Raviteja Kolipaka <kolipakaraviteja@gmail.com>
2026-08-30 19:27:16 +0530

    url update

--- src/environments/environment.render.ts
+++ src/environments/environment.render.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://ai-knowledge-engine-2lmc.onrender.com',
-  agentPlatformUrl: 'https://ai-knowledge-engine-2lmc.onrender.com'
+  agentPlatformUrl: 'https://ai-agent-platform-pk5t.onrender.com'
 };
